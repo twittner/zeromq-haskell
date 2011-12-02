68d3c4c731e2bee1676d21b3e813a8f2a9a8b099
Toralf Wittner <toralf.wittner@gmail.com>
2011-12-02 22:43:59 +0100

    Add more tests.

--- tests/tests.hs
+++ tests/tests.hs
@@ -9,13 +9,11 @@ main = defaultMain tests
 
 tests :: [Test]
 tests = [
-    testGroup "Init/Exit" [
-        testCase "ZMQ Context" test_context
+    testGroup "Misc" [
+          testCase "ZMQ Context" test_context
     ]
   , testGroup "Socket Creation" [
-          testCase "Down" (test_socket ZMQ.Down)
-        , testCase "Up"   (test_socket ZMQ.Up)
-        , testCase "Push" (test_socket ZMQ.Push)
+          testCase "Push" (test_socket ZMQ.Push)
         , testCase "Pull" (test_socket ZMQ.Pull)
         , testCase "XRep" (test_socket ZMQ.XRep)
         , testCase "XReq" (test_socket ZMQ.XReq)
@@ -26,6 +24,10 @@ tests = [
         , testCase "Sub"  (test_socket ZMQ.Sub)
         , testCase "Pub"  (test_socket ZMQ.Pub)
         , testCase "Pair" (test_socket ZMQ.Pair)
+--ifdef ZMQ2
+        , testCase "Down" (test_socket ZMQ.Down)
+        , testCase "Up"   (test_socket ZMQ.Up)
+--endif
     ]
   , testGroup "Socket Option (get)" [
           testCase "Affinity"        (test_option_get (ZMQ.Affinity undefined))
@@ -41,10 +43,39 @@ tests = [
         , testCase "ReconnectIVLMax" (test_option_get (ZMQ.ReconnectIVLMax undefined))
         , testCase "ReconnectIVL"    (test_option_get (ZMQ.RecoveryIVL undefined))
         , testCase "SendBuf"         (test_option_get (ZMQ.SendBuf undefined))
+--ifdef ZMQ2
         , testCase "HighWM"          (test_option_get (ZMQ.HighWM undefined))
         , testCase "McastLoop"       (test_option_get (ZMQ.McastLoop undefined))
         , testCase "RecoveryIVLMsec" (test_option_get (ZMQ.RecoveryIVLMsec undefined))
         , testCase "Swap"            (test_option_get (ZMQ.Swap undefined))
+--endif
+    ]
+  , testGroup "Socket Option (set)" [
+          testCase "Affinity"        (test_option_get (ZMQ.Affinity 0))
+        , testCase "Backlog"         (test_option_get (ZMQ.Backlog 100))
+        , testCase "Events"          (test_option_get (ZMQ.Events undefined))
+        , testCase "FD"              (test_option_get (ZMQ.FD undefined))
+        , testCase "Identity"        (test_option_get (ZMQ.Identity "id"))
+        , testCase "Linger"          (test_option_get (ZMQ.Linger (-1)))
+        , testCase "Rate"            (test_option_get (ZMQ.Rate 100))
+        , testCase "ReceiveBuf"      (test_option_get (ZMQ.ReceiveBuf 0))
+        , testCase "ReceiveMore"     (test_option_get (ZMQ.ReceiveMore undefined))
+        , testCase "ReconnectIVL"    (test_option_get (ZMQ.ReconnectIVL 100))
+        , testCase "ReconnectIVLMax" (test_option_get (ZMQ.ReconnectIVLMax 0))
+        , testCase "ReconnectIVL"    (test_option_get (ZMQ.RecoveryIVL 100))
+        , testCase "SendBuf"         (test_option_get (ZMQ.SendBuf 0))
+--ifdef ZMQ2
+        , testCase "HighWM"          (test_option_get (ZMQ.HighWM 0))
+        , testCase "McastLoop"       (test_option_get (ZMQ.McastLoop True))
+        , testCase "RecoveryIVLMsec" (test_option_get (ZMQ.RecoveryIVLMsec (-1)))
+        , testCase "Swap"            (test_option_get (ZMQ.Swap 0))
+--endif
+    ]
+  , testGroup "Pub/Sub" [
+          testCase "Subscribe Sub"    (test_subscribe ZMQ.Sub)
+        , testCase "Subscribe XSub"   (test_subscribe ZMQ.XSub)
+        , testCase "Unsubscribe Sub"  (test_unsubscribe ZMQ.Sub)
+        , testCase "Unsubscribe XSub" (test_unsubscribe ZMQ.XSub)
     ]
   ]
 
@@ -63,6 +94,23 @@ test_socket ty =
 test_option_get :: ZMQ.SocketOption -> IO ()
 test_option_get o =
     ZMQ.withContext 1 $ \c ->
-        ZMQ.withSocket c ZMQ.XSub $ \s ->
-            ZMQ.getOption s o >> return ()
+    ZMQ.withSocket c ZMQ.XSub $ \s ->
+        ZMQ.getOption s o >> return ()
 
+test_option_set :: ZMQ.SocketOption -> IO ()
+test_option_set o =
+    ZMQ.withContext 1 $ \c ->
+    ZMQ.withSocket c ZMQ.XSub $ \s ->
+        ZMQ.setOption s o
+
+test_subscribe :: (ZMQ.SubsType a, ZMQ.SType a) => a -> IO ()
+test_subscribe ty =
+    ZMQ.withContext 1 $ \c ->
+        ZMQ.withSocket c ty $ \s ->
+            ZMQ.subscribe s ""
+
+test_unsubscribe :: (ZMQ.SubsType a, ZMQ.SType a) => a -> IO ()
+test_unsubscribe ty =
+    ZMQ.withContext 1 $ \c ->
+        ZMQ.withSocket c ty $ \s ->
+            ZMQ.unsubscribe s ""
