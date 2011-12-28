aceefd2f1634f26b3f66f9016daa13d52dda0331
Toralf Wittner <toralf.wittner@gmail.com>
2011-12-28 21:45:08 +0100

    Add QuickCheck Properties.

--- tests/System/ZMQ3/Test/Properties.hs
+++ tests/System/ZMQ3/Test/Properties.hs
@@ -20,8 +20,31 @@ import qualified Data.ByteString.Char8 as CB
 tests :: [Test]
 tests = [
     testGroup "0MQ Socket Properties" [
-        testProperty "set/get socket option" (prop_set_get_socket_option Pair)
-      , testProperty "(un-)subscribe"        (prop_subscribe Sub)
+        testProperty "get socket option (Pair)" (prop_get_socket_option Pair)
+      , testProperty "get socket option (Pub)"  (prop_get_socket_option Pub)
+      , testProperty "get socket option (Sub)"  (prop_get_socket_option Sub)
+      , testProperty "get socket option (XPub)" (prop_get_socket_option XPub)
+      , testProperty "get socket option (XSub)" (prop_get_socket_option XSub)
+      , testProperty "get socket option (Req)"  (prop_get_socket_option Req)
+      , testProperty "get socket option (Rep)"  (prop_get_socket_option Rep)
+      , testProperty "get socket option (XReq)" (prop_get_socket_option XReq)
+      , testProperty "get socket option (XRep)" (prop_get_socket_option XRep)
+      , testProperty "get socket option (Pull)" (prop_get_socket_option Pull)
+      , testProperty "get socket option (Push)" (prop_get_socket_option Push)
+
+      , testProperty "set/get socket option (Pair)" (prop_set_get_socket_option Pair)
+      , testProperty "set/get socket option (Pub)"  (prop_set_get_socket_option Pub)
+      , testProperty "set/get socket option (Sub)"  (prop_set_get_socket_option Sub)
+      , testProperty "set/get socket option (XPub)" (prop_set_get_socket_option XPub)
+      , testProperty "set/get socket option (XSub)" (prop_set_get_socket_option XSub)
+      , testProperty "set/get socket option (Req)"  (prop_set_get_socket_option Req)
+      , testProperty "set/get socket option (Rep)"  (prop_set_get_socket_option Rep)
+      , testProperty "set/get socket option (XReq)" (prop_set_get_socket_option XReq)
+      , testProperty "set/get socket option (XRep)" (prop_set_get_socket_option XRep)
+      , testProperty "set/get socket option (Pull)" (prop_set_get_socket_option Pull)
+      , testProperty "set/get socket option (Push)" (prop_set_get_socket_option Push)
+
+      , testProperty "(un-)subscribe"               (prop_subscribe Sub)
       ]
   , testGroup "0MQ Messages" [
         testProperty "msg send == msg received (Req/Rep)"   (prop_send_receive Req Rep)
@@ -29,6 +52,15 @@ tests = [
       , testProperty "msg send == msg received (Pair/Pair)" (prop_send_receive Pair Pair)
       ]
   ]
+
+prop_get_socket_option :: SType t => t -> GetOpt -> Property
+prop_get_socket_option t opt = monadicIO $ run $ do
+    withContext 1 $ \c ->
+        withSocket c t $ \s ->
+            case opt of
+                Events _      -> events s         >> return ()
+                Filedesc _    -> fileDescriptor s >> return ()
+                ReceiveMore _ -> moreToReceive s  >> return ()
 
 prop_set_get_socket_option :: SType t => t -> SetOpt -> Property
 prop_set_get_socket_option t opt = monadicIO $ do
@@ -81,56 +113,63 @@ instance Arbitrary ByteString where
 
 data GetOpt =
     Events          Int
-  | ReceiveMore     Bool
   | Filedesc        Fd
+  | ReceiveMore     Bool
   deriving Show
 
 data SetOpt =
     Affinity        Word64
   | Backlog         (Restricted N0 Int32 Int)
   | Identity        (Restricted N1 N254 String)
+  | Ipv4Only        Bool
   | Linger          (Restricted Nneg1 Int32 Int)
+  | MaxMessageSize  Int64
+  | McastHops       (Restricted N1 Int32 Int)
   | Rate            (Restricted N1 Int32 Int)
   | ReceiveBuf      (Restricted N0 Int32 Int)
+  | ReceiveHighWM   (Restricted N0 Int32 Int)
+  | ReceiveTimeout  (Restricted Nneg1 Int32 Int)
   | ReconnectIVL    (Restricted N0 Int32 Int)
   | ReconnectIVLMax (Restricted N0 Int32 Int)
   | RecoveryIVL     (Restricted N0 Int32 Int)
   | SendBuf         (Restricted N0 Int32 Int)
-  | Ipv4Only        Bool
-  | MaxMessageSize  Int64
-  | McastHops       (Restricted N1 Int32 Int)
-  | ReceiveHighWM   (Restricted N0 Int32 Int)
-  | ReceiveTimeout  (Restricted Nneg1 Int32 Int)
   | SendHighWM      (Restricted N0 Int32 Int)
   | SendTimeout     (Restricted Nneg1 Int32 Int)
   deriving Show
 
+instance Arbitrary GetOpt where
+    arbitrary = oneof [
+        Events                       <$> arbitrary
+      , Filedesc . Fd . fromIntegral <$> (arbitrary :: Gen Int32)
+      , ReceiveMore                  <$> arbitrary
+      ]
+
 instance Arbitrary SetOpt where
     arbitrary = oneof [
-        Identity . fromJust . restrict . show <$> arbitrary `suchThat` (\s -> SB.length s > 0 && SB.length s < 255)
-      , Affinity              <$> (arbitrary :: Gen Word64)
-      , Ipv4Only              <$> (arbitrary :: Gen Bool)
-      , Backlog         . toR0 <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
+        Affinity                  <$> (arbitrary :: Gen Word64)
+      , Ipv4Only                  <$> (arbitrary :: Gen Bool)
+      , Backlog         . toR0    <$> (arbitrary :: Gen Int32) `suchThat` (>=  0)
       , Linger          . toRneg1 <$> (arbitrary :: Gen Int32) `suchThat` (>= -1)
-      , Rate            . toR1 <$> (arbitrary :: Gen Int32) `suchThat` (> 0)
-      , ReceiveBuf      . toR0 <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
-      , ReconnectIVL    . toR0 <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
-      , ReconnectIVLMax . toR0 <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
-      , RecoveryIVL     . toR0 <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
-      , SendBuf         . toR0 <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
-      , MaxMessageSize  . fromIntegral <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
-      , McastHops       . toR1 <$> (arbitrary :: Gen Int32) `suchThat` (> 0)
-      , ReceiveHighWM   . toR0 <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
+      , Rate            . toR1    <$> (arbitrary :: Gen Int32) `suchThat` (>   0)
+      , ReceiveBuf      . toR0    <$> (arbitrary :: Gen Int32) `suchThat` (>=  0)
+      , ReconnectIVL    . toR0    <$> (arbitrary :: Gen Int32) `suchThat` (>=  0)
+      , ReconnectIVLMax . toR0    <$> (arbitrary :: Gen Int32) `suchThat` (>=  0)
+      , RecoveryIVL     . toR0    <$> (arbitrary :: Gen Int32) `suchThat` (>=  0)
+      , SendBuf         . toR0    <$> (arbitrary :: Gen Int32) `suchThat` (>=  0)
+      , McastHops       . toR1    <$> (arbitrary :: Gen Int32) `suchThat` (>   0)
+      , ReceiveHighWM   . toR0    <$> (arbitrary :: Gen Int32) `suchThat` (>=  0)
       , ReceiveTimeout  . toRneg1 <$> (arbitrary :: Gen Int32) `suchThat` (>= -1)
-      , SendHighWM      . toR0 <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
+      , SendHighWM      . toR0    <$> (arbitrary :: Gen Int32) `suchThat` (>=  0)
       , SendTimeout     . toRneg1 <$> (arbitrary :: Gen Int32) `suchThat` (>= -1)
+      , MaxMessageSize  . fromIntegral <$> (arbitrary :: Gen Int32) `suchThat` (>= 0)
+      , Identity . fromJust . toRestricted . show <$> arbitrary `suchThat` (\s -> SB.length s > 0 && SB.length s < 255)
       ]
 
 toR1 :: Int32 -> Restricted N1 Int32 Int
-toR1 = fromJust . restrict . fromIntegral
+toR1 = fromJust . toRestricted . fromIntegral
 
 toR0 :: Int32 -> Restricted N0 Int32 Int
-toR0 = fromJust . restrict . fromIntegral
+toR0 = fromJust . toRestricted . fromIntegral
 
 toRneg1 :: Int32 -> Restricted Nneg1 Int32 Int
-toRneg1 = fromJust . restrict . fromIntegral
+toRneg1 = fromJust . toRestricted . fromIntegral
